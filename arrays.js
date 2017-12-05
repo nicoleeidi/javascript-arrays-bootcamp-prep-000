5093a1ef73fc29bb7d3893715439f46e0af40556
Nicole Eidi <nicoleeidi1@gmail.com>
2017-12-05 11:12:48 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars= ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array,element){
+    return array=[element,...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+    return array.unshift(element)
+}
+function addElementToEndOfArray(array,element){
+    return array=[...array,element]
+  }
+
+function destructivelyAddElementToEndOfArray(array,element){
+    return array.push(element)
+
+  }
+function accessElementInArray(array,index){
+        return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+    return array=array.shift()
+}
+
+function removeElementFromBeginningOfArray(array){
+    return array= array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+    return array.pop()
+}
+function removeElementFromEndOfArray(array){
+    return array= array.slice(0,array.length-1)
+}
