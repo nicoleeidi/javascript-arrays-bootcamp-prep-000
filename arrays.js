00841c0b5c9e38b6cf5aa5568400a0b2b2c80fe7
Nicole Eidi <nicoleeidi1@gmail.com>
2017-12-05 12:01:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function accessElementInArray(array,index){
         return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array){
-    array=array.shift()
+    array.shift()
     return array
 }
 
